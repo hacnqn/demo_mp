9877aec04beecb1868ea8fa7a1a440f718160409
hacnqn <65201902+hacnqn@users.noreply.github.com>
2020-05-11 21:19:50 -0300

    Script de consulta publico

---
+++ demo_mp.py
@@ -0,0 +1,33 @@
+import sys
+import requests	
+"""
+1 Inicio su entorno python
+2 ejecute script python demo_mp <id_vendedor> ej: python demo_mp 179571326
+3 Recupere salida en ./
+"""
+def demo_mp(_param):
+    results =[]
+    file_out = open(r"demo-log.txt","w+")    
+    response = requests.get('https://api.mercadolibre.com/sites/MLA/search?seller_id='+_param)
+    data = response.json()
+    items = data['results']
+    #print(items)
+    print ('procesando...')
+    cant_items=len(items)
+    index=0
+    while index < cant_items:
+        id=str(items[index]['id'])
+        title = str(items[index]['title'])
+        category_id=str(items[index]['category_id'])
+        response2 = requests.get('https://api.mercadolibre.com/categories/'+category_id)
+        data2 = response2.json()
+        category_name = str(data2['name'])
+        str1 = id +';'+title+';'+category_id+';'+ category_name
+        file_out.write('{}\n'.format(str1))       
+        index+=1 
+    print ('fin')
+    file_out.close()
+	
+if __name__ == "__main__":
+    id = str(sys.argv[1])
+    demo_mp(id)
---
+++ readme.txt
@@ -0,0 +1,14 @@
+-------------------------------------------------
+--  Consulta de Items por vendedor
+--  Solo aplica a MLA
+--  para python 3.7+
+-------------------------------------------------
+
+input: seller_id
+output: txt separado por ; conteniendo: id, title, category_id, name
+
+Para ejecutar, desde su consola hacer:
+python demo_mp <seller_id>
+
+para mutiples seller solo incluir en un bucle
+
